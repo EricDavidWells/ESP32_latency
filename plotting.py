99c59b6dc40d477eb5c947c4ba654289735b9edc
Eric Wells <ewells@ualberta.ca>
2020-07-17 00:14:10 -0600

    added logit scaling to plots, changed x-axis to cumulative percentage

--- plotting.py
+++ plotting.py
@@ -1,5 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib.ticker import NullFormatter  # useful for `logit` scale
 
 
 def latency_plot(filename, maxvalue=None):
@@ -19,14 +20,36 @@ def latency_plot(filename, maxvalue=None):
     plt.text(x, y, "Mean Value: " + str(np.mean(data)), horizontalalignment='center', verticalalignment='center')
 
 
+def latency_plot_log(filename, maxvalue=None):
+
+    data = np.genfromtxt(filename, delimiter=',')
+    data = data[np.logical_not(np.isnan(data))] # remove nan entries
+    if maxvalue is None:
+        maxvalue = max(data)
+
+    # print(len(data))
+    # print(sum(data))
+    data_range = (min(data), max(data))
+
+    y = np.linspace(data_range[0], data_range[1], 10000)
+    x = np.zeros(y.shape)
+    for i, val in enumerate(y):
+        x_mask = data <= val
+        count = np.sum(x_mask)
+        x[i] = count/data.size
+
+    plt.figure(1)
+    plt.xscale('logit')
+    plt.plot(x, y)
+    plt.gca().xaxis.set_minor_formatter(NullFormatter())
+
+
 def throughput_plot(filename, maxvalue=None):
     data = np.genfromtxt(filename, delimiter=',')
     data = data[np.logical_not(np.isnan(data))] # remove nan entries
 
     if maxvalue is None:
         maxvalue = max(data)
-
-    print(maxvalue)
 
     plt.figure()
 
@@ -39,8 +62,35 @@ def throughput_plot(filename, maxvalue=None):
     plt.text(x, y, "Mean Value: " + str(np.mean(data)), horizontalalignment='center', verticalalignment='center')
 
 
+def throughput_plot_log(filename, maxvalue=None):
+
+    data = np.genfromtxt(filename, delimiter=',')
+    data = data[np.logical_not(np.isnan(data))]  # remove nan entries
+    if maxvalue is None:
+        maxvalue = max(data)
+
+    # print(len(data))
+    # print(sum(data))
+    data_range = (min(data), max(data))
+
+    y = np.linspace(data_range[0], data_range[1], 10000)
+    x = np.zeros(y.shape)
+    for i, val in enumerate(y):
+        x_mask = data <= val
+        count = np.sum(x_mask)
+        x[i] = count/data.size
+
+    plt.figure(1)
+    plt.xscale('logit')
+    plt.plot(x, y)
+    plt.gca().xaxis.set_minor_formatter(NullFormatter())
+
+
 if __name__ == "__main__":
-    maxvalue =  latency_plot("latency_test_16B_100s_1m.txt")
+    throughput_plot_log("throughput_test_16B_100s_1m.txt")
+    latency_plot_log("latency_test_16B_100s_1m.txt")
+
     throughput_plot("throughput_test_16B_100s_1m.txt")
+    latency_plot("latency_test_16B_100s_1m.txt")
     plt.show()
 
